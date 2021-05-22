a2cebc12522af855fd2dcccd562582781880f504
DeniseLuiz <denisesantos.luiz@usp.br>
2021-05-21 21:41:34 -0300

    model-interfaces

---
+++ ProffyApi/ProffyApi/Model/Proffy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProffyApi.Model
+{
+    public class Proffy
+    {
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public int Whatsapp { get; set; }
+        public string Biografy { get; set; }
+        public string Subject { get; set; }
+        public double Cost { get; set; }
+        public int Weekday { get; set; }
+        public int TimeFrom { get; set; }
+        public int TimeTo { get; set; }
+    }
+}
